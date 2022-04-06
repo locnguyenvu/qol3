7d07b4afce002bca3a181b183d69c840100c269e
locnguyenvu <ngynvuloc@gmail.com>
2022-04-06 22:47:00 +0700

    Arp-2022: Update 3, screenshot vnindex

--- qol3/cli/__init__.py
+++ qol3/cli/__init__.py
@@ -1,5 +1,7 @@
 import click
+import asyncio
 from flask.cli import AppGroup, with_appcontext
+from pyppeteer import launch
 
 from . import bot, config, dcvfm
 
@@ -21,4 +23,22 @@ def init_app(app):
     app.cli.add_command(AppGroup("dcvfm", commands=[
         click.Command("nav-today", callback=with_appcontext(dcvfm.nav_today)),
     ]))
+
+    app.cli.add_command(click.Command("test", callback=real_test))
     pass
+
+
+async def test():
+    browser = await launch()
+    page = await browser.newPage()
+    await page.setViewport(dict(width=1000, height=1200, isMobile=False))
+    await page.goto('https://www.msn.com/vi-vn/money/indexdetails/fi-aqk2nm?duration=1D')
+    await asyncio.sleep(3)
+    # await page.screenshot({'path': 'vnindex-original.png'})
+    await page.screenshot({'path': 'vnindex-clip.png', 'clip': {'x': 0, 'y': 202.0, 'width': 950, 'height': 798}})
+    await browser.close()
+
+
+def real_test():
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(test())
